45a845c3a85a2c4aeacd6bb9528dfd6d31ad9710
soham <sohamkamani@gmail.com>
2016-07-16 14:18:52 +0000

    added more

--- get-serial.js
+++ get-serial.js
@@ -2,7 +2,16 @@ var SerialPort = require("serialport").SerialPort;
 var serialport = new SerialPort("/dev/ttyACM0");
 serialport.on('open', function() {
   console.log('Serial Port Opend');
+  let dataString = '';
+  const showData = ()=>{
+	console.log(dataString);
+	dataString = '';
+  }
   serialport.on('data', function(data) {
-    console.log(data[0]);
+	const ds = data.toString();
+    dataString += ds;
+    if(dataString.indexOf('End') >= 0 ){
+		showData()
+	}
   });
 });
