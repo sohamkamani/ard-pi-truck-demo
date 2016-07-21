27b239397d464865136df9f6e2887dba231ede3f
sohamkamani <sohamkamani@gmail.com>
2016-07-21 18:09:48 +0530

    Added vue

--- public/main.js
+++ public/main.js
@@ -1,3 +1,34 @@
 var socket = io();
 
-socket.on('state-change', newState => console.log(newState));
+const data = {
+  box1: null,
+  box2: null,
+  totalWeight: '0',
+  rfid: 'N/A'
+};
+
+const App = new Vue({
+  el: '#app',
+  data: data
+})
+
+const convertToBinary = d => Boolean(Number(d))
+
+const getWeight = (box1, box2) => {
+  let weight = 0;
+  if (box1) {
+    weight += 2
+  }
+  if (box2) {
+    weight += 3
+  }
+  return weight
+}
+
+socket.on('state-change', newState => {
+  console.log(newState)
+  newState.box1 = convertToBinary(newState.box1)
+  newState.box2 = convertToBinary(newState.box2)
+  newState.totalWeight = getWeight(newState.box1, newState.box2)
+  Object.assign(data, newState)
+});
--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const {subscribe} = require('./get-serial');
 app.use(express.static('public'));
 
 io.on('connection', function(socket) {
-  // socket.on('chat message', function(msg) {
-  //   io.emit('chat message', msg);
-  // });
+
   console.log('Connection established');
 
   const onStateChange = state => io.emit('state-change', state)
   subscribe(onStateChange)
 });
 
-setTimeout(() => io.emit('state-change', 'deded'), 4000)
+// setInterval(() => io.emit('state-change', {
+//   box1: '1',
+//   box2: '0',
+//   rfid: 'ASS232340'
+// }), 4000)
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
